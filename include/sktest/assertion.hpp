d5646976e365196c2499c9c281df1e9b1c11f7db
Kk Shinkai <kkshinkai@outlook.com>
2022-01-05 04:22:56 +0800

    Overloading macro `require` on number of arguments

--- include/sktest/assertion.hpp
+++ include/sktest/assertion.hpp
@@ -6,15 +6,31 @@
 #ifndef sktest_require_impl
 #define sktest_require_impl(expr, description, file, line)                     \
   if (!(expr)) {                                                               \
-    printf("\033[31;1;1merror:\033[0m test failed at %s:%d\n", file, line);    \
-    printf(" \033[37m%d |\033[0m  require(\033[34m%s\033[0m)\n", line, #expr); \
-    printf(" \033[37m%d |\033[0m  ^^^^^^^%s\n", line + 1, description);        \
+    printf("\033[31;1;1merror:\033[0m test failed at %s:%  d\n", file, line);  \
+    printf("  require( \033[34m%s\033[0m )\n", #expr);                         \
+    printf("  ^^^^^^^%s\n", description);                                      \
   }
 #endif
 
-#ifndef require
-#define require(expr, description)                                             \
+#ifndef sktest_require_with_description
+#define sktest_require_with_description(expr, description)                     \
   sktest_require_impl(expr, description, __FILE__, __LINE__)
+#endif
+
+#ifndef sktest_require_without_description
+#define sktest_require_without_description(expr)                               \
+  sktest_require_impl(expr, "", __FILE__, __LINE__)
+#endif
+
+#ifndef sktest_get_macro
+#define sktest_get_macro(_1, _2, name, ...) name
+#endif
+
+#ifndef require
+#define require(...)                                                           \
+  sktest_get_macro(__VA_ARGS__,                                                \
+                   sktest_require_with_description,                            \
+                   sktest_require_without_description)(__VA_ARGS__)
 #endif
 
 #endif /* _sktest_assertion_hpp */
